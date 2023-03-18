23a0a0c1ffad248508337e9e3a2e4c961c5a6d6c
rakeshrengaraj <rakeshrengaraj@bitbucket.org>
2023-03-18 23:27:31 +0530

    Updated Swipe model

--- src/models/swipe.ts
+++ src/models/swipe.ts
@@ -5,12 +5,16 @@ export interface ISwipe {
   userA: Schema.Types.ObjectId | IUserPreference;
   userB: Schema.Types.ObjectId | IUserPreference;
   swipeState: string;
+  isMatch: boolean;
 }
 
 const swipeSchema = new Schema<ISwipe>({
   userA: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
   userB: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
-  swipeState: { type: String, required: true },
+  swipeState: { type: String, required: true, enum: ['left', 'right'] },
+  isMatch: {type: Boolean, default: false}
 });
 
 export const Swipe = model<ISwipe>('Swipe', swipeSchema);
+
+
